5f536870473f646155baab1cc905d4b3d1763935
Shivamya94 <yadavshivam90440@gmail.com>
2024-08-11 18:08:36 +0530

    commit first file

---
+++ demoshopapp/src/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        id: 1,
+        name: "Agra",
+        info: "Agra is a city in northern India, known for the iconic Taj Mahal,a stunning white marbel mausoeum built by Mughal emperor Shah Jahan in Memeory of his beloved wife.Agra is also home to several other.Agra is a city in northern India, known for the iconic Taj Mahal,a stunning white marbel mausoeum built by Mughal emperor Shah Jahan in Memeory of his beloved wife.Agra is also home to several other",
+        image: "https://whc.unesco.org/uploads/thumbs/site_0252_0008-750-750-20151104113424.jpg",
+        price: "33,760",
+    },
+    {
+        id: 2,
+        name: "jaipur",
+        info: "Jaipur is the Capital city of the Indian state of Rajasthan,Known as the pink city.Jaipur is the Capital city of the Indian state of Rajasthan,Known as the pink city",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/41/East_facade_Hawa_Mahal_Jaipur_from_ground_level_%28July_2022%29_-_img_01.jpg",
+        price: "89,900",
+    },
+    {
+        id: 3,
+        name: "Goa",
+        info: "Goa is a small state on India's western coast,known for its .Goa is a small state on India's western coast,known for its.Goa is a small state on India's western coast,known for its",
+        image: "https://www.moneytap.com/blog/wp-content/uploads/2023/04/cost-of-living-in-goa-scaled.jpeg",
+        price: "29,900",
+    },
+    {
+        id: 4,
+        name: "Varanasi",
+        info: "Varanasi is a only city is nothern India,situated on the backs of .Goa is a small state on India's western coast,known for its.Goa is a small state on India's western coast,known for its ",
+        image: "https://upload.wikimedia.org/wikipedia/commons/0/04/Ahilya_Ghat_by_the_Ganges%2C_Varanasi.jpg",
+        price: "79,900",
+    },
+    {
+        id: 5,
+        name: "Darjeeling",
+        info: "starting in the colorful port city of Gdansk, you'll escape the crown.Goa is a small state on India's western coast,known for itsGoa is a small state on India's western coast,known for itsGoa is a small state on India's western coast,known for its",
+        image: "https://1001things.org/wp-content/uploads/2023/02/1001-things-copy.jpg",
+        price: "76,900",
+    },
+    {
+        id: 6,
+        name: "Jaisalmer",
+        info: "Jaisalmer is a city in the Indian state of Rajasthan ,known for its.Goa is a small state on India's western coast,known for itsGoa is a small state on India's western coast,known for itsGoa is a small state on India's western coast,known for its",
+        image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/09/Thar-Desert.jpg",
+        price: "68,900",
+    },
+    {
+        id: 7,
+        name: "Kochi",
+        info: " Kochi is an enchanting city situated on the southwest coast of India in the state of Kerala. Popularly known as 'Queen of the Arabian Sea.Goa is a small state on India's western coast,known for itsGoa is a small state on India's western coast,known for itsGoa is a small state on India's western coast,known for its",
+        image: "https://www.keralatourism.org/images/microsites/kochi/fort-kochi.jpg",
+        price: "65,900",
+    },
+     
+];
+
+export default data;
