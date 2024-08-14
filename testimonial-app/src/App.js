2d32bda83d39f88bc4c5594a2e3897f7042eeb5b
Shivamya94 <yadavshivam90440@gmail.com>
2024-08-14 17:04:47 +0530

    Created a Testimonial-app

---
+++ testimonial-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>Our Testimonials</h1>
+        <div></div>
+        <Testimonials/>
+      </div>
+
+    </div>
+
+  );
+};
+export default App;
---
+++ testimonial-app/src/data.js
@@ -0,0 +1,38 @@
+const reviews =[
+    {
+        id: 1,
+        name: "Pranay Gupta",
+        job: "Software Engineer",
+        image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text: "I have had the pleasure of working with this team or several projects,and My expertise spans across various programming languages, frameworks, and technologies, with a strong focus on delivering scalable and maintainable code."
+    },
+    {
+        id: 2,
+        name: "Abir Pal",
+        job: "Graphic Designer",
+        image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+        text: "I have been working with this company for several years now, and My expertise lies in translating concepts into impactful designs that effectively communicate brand messages and engage target audiences. "
+    },
+    {
+        id: 3,
+        name: "Soumya Banerjee",
+        job: "Marketing Manager",
+        image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text: "I am thrilled with the results of our recent marketing campaign ,and it wouldn't have"
+    },
+    {
+        id: 4,
+        name: "Saikat Mukherjee",
+        job: "Content Strategist",
+        image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+        text: "I have worked with many content creators over the years ,but none have impressed me "
+    },
+    {
+        id: 5,
+        name: "Aritra Biswas",
+        job: "Data Analyst",
+        image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+        text: "I had the opportunity to work with this team on a data analysis project,and I was in "
+    },
+    
+]
