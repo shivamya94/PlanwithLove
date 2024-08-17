9dc0438b05bfd18ab557ad0a7bf2700c3408e90b
Shivamya94 <yadavshivam90440@gmail.com>
2024-08-17 15:49:28 +0530

    added css configuration

--- testimonial-app/src/components/Card.js
+++ testimonial-app/src/components/Card.js
@@ -1,45 +1,37 @@
 import React from 'react'
 import {FaQuoteLeft, FaQuoteRight, } from "react-icons/fa";
 
+
  const Card = (props) => {
     let review = props.review;
   return (
-    <div>
-        <div>
-            <img src={review.image}/>
+    <div className='flex flex-col md:relative'>
+        <div className='absolute top-[-7rem] z-[10] mx-auto'>
+            <img
+            className='aspect-square rounded-full w-[140px] h-[140px] z-[25]'
+            src={review.image}/>
+            <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute
+             top-[-6px] z-[-10] left-[10px]'></div>
+
         </div>
-        <div>
-            <p>{review.name}</p>
+        <div className='text-center mt-7'>
+            <p className=' tracking-wider font-bold text-2xl capitalize'>{review.name}</p>
+            <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
         </div>
 
-        <div>
-            <p>{review.job}</p>
-        </div>
-
-        <div>
+        <div className='text-violet-400 mx-auto mt-5'>
             <FaQuoteLeft/>
         </div>
-        <div>
+
+        <div className='text-center mt-4 text-slate-500'>
             {review.text}
         </div>
 
-        <div>
+        <div className='text-violet-400 mx-auto mt-5'>
             <FaQuoteRight/>
         </div>
-        <div>
-            <button>
-                *
-            </button>
-            <button>
-                *
-            </button>
-        </div>
 
-        <div>
-            <button>
-                Surprise Me
-            </button>
-        </div>
+        
     </div>
   )
 }
