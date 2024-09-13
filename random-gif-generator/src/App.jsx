46e390702a797236effcc7b182925056ea72c2c2
Shivamya94 <yadavshivam90440@gmail.com>
2024-09-13 21:18:37 +0530

    added

--- random-gif-generator/src/App.jsx
+++ random-gif-generator/src/App.jsx
@@ -5,9 +5,11 @@ import Tag from "./components/Tag";
 export default function App() {
     return (
 
-        <div className="w-full h-screen flex-col background relative">
-            <h1 className="absolute bg-white rounded-sm w-full text-center mt-[40px]">RANDOM GIFS</h1>
-             <div className="flex flex-col">
+        <div className="w-full h-screen flex-col background relative overflow-x-hidden item-center">
+            <h1 className=" bg-white rounded-lg w-11/12 text-center mt-[40px]
+            px-10 py-2 text-4xl font-bold">
+            RANDOM GIFS</h1>
+            <div className="flex flex-col">
                 <Random/>
                 <Tag/>
              </div>
