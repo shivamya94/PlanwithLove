478ae45f1d1cb5fca3c3ff84e2f3c33e2fad733e
Shivamya94 <yadavshivam90440@gmail.com>
2024-09-13 17:00:41 +0530

    .

--- random-gif-generator/src/App.jsx
+++ random-gif-generator/src/App.jsx
@@ -2,11 +2,9 @@ export default function App() {
     return (
 
         <div className="w-full h-screen flex-col background ">
-            <h1 className="">RANDOM GIFS</h1>
+            <h1 className="bg-white rounded-sm">RANDOM GIFS</h1>
              <div className="flex flex-col">
-                <Random />
-                <Tag />
              </div>
         </div>
-    )
+    );
 }
