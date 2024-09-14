5eae25159a4d2dc04b1176f4f0a46a2baed867a6
Shivamya94 <yadavshivam90440@gmail.com>
2024-09-14 20:03:19 +0530

    .

--- random-gif-generator/src/App.jsx
+++ random-gif-generator/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
             <h1 className=" bg-white rounded-lg w-11/12 text-center mt-[40px]
              px-10 py-2 text-4xl font-bold">
             RANDOM GIFS</h1>
-            <div className="flex flex-col items-center w-full">
+            <div className="flex flex-col items-center w-full gap-y-10 mt-[30px]">
                 <Random/>
                 <Tag/>
              </div>
--- random-gif-generator/src/components/Random.js
+++ random-gif-generator/src/components/Random.js
@@ -13,10 +13,14 @@ const Random = ()=> {
 
 
   return (
-    <div className='w-1/2 h-[450px] bg-green-500'>
-        <h1>Random Gif</h1>
+    <div className='w-1/2 h-[450px] bg-green-500 rounded-lg border border-black
+     flex flex-col items-center gap-y-5 mt-[15px] '>
+        <h1 className='text-2xl underline uppercase font-bold'>A Random Gif</h1>
+
         <img src ={gif} width="450"/>
-        <button onClick={clickHandler}>
+
+        <button onClick={clickHandler}
+        className='w-10/12 bg-yellow-500 text-lg py-2 rounded-lg'>
         Generate
         </button>
     </div>
