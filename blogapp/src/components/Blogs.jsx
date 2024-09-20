7fa5ad2048710443f4b7aa387039c07b5457b193
Shivamya94 <yadavshivam90440@gmail.com>
2024-09-20 18:40:12 +0530

    ..

--- blogapp/src/components/Blogs.jsx
+++ blogapp/src/components/Blogs.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Blogs = () => {
+    //consume
+    const {posts,loading} = useContext(AppContext);
   return (
-    <div></div>
+    <div>
+        {
+            loading ? 
+
+            (<Spinner />) : 
+            (
+                posts.length == 0 ? (
+                <div>
+                    <p>No Post Found</p>
+                </div>) : 
+                (posts.map( (post) => (<Card />)))
+            )
+        }
+
+    </div>
   )
 }
 
