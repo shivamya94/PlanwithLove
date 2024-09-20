4900b08b0cba967a1a63056664756cde347d7f16
Shivamya94 <yadavshivam90440@gmail.com>
2024-09-20 21:59:27 +0530

    ..

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -1,8 +1,16 @@
 import Header from "./components/Header";
 import Blogs from "./components/Blogs";
 import Pagination from "./components/Pagination";
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
+import "./App.css"
 
 export default function App() {
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect( () => {
+    fetchBlogPosts();
+  },[]);
   return (
     <div>
       <Header />
--- blogapp/src/components/Blogs.jsx
+++ blogapp/src/components/Blogs.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
+import Spinner from './Spinner'
 
 const Blogs = () => {
     //consume
     const {posts,loading} = useContext(AppContext);
+    console.log("Printig inside blogs components");
+    console.log(posts);
+
   return (
     <div>
         {
             loading ? 
 
-            (<Spinner />) : 
+            (<Spinner/>) : 
             (
                 posts.length == 0 ? (
                 <div>
                     <p>No Post Found</p>
                 </div>) : 
-                (posts.map( (post) => (<Card />)))
+                (posts.map( (post) => (
+                    <div key ={post.id}>
+                        <p className=' text-xl text-blue-600'>{post.title}</p> 
+                        <p>
+                            By<span>{post.author}</span> on <span>{post.category}</span>
+                        </p>
+                        <p>Posted on {post.date}</p>
+                        <p>{post.content}</p>
+                        <div> 
+                            {post.tags.map( (tag, index) => {
+                                return <span key={index}>{`#${tag}`}</span>
+                            })}
+
+                        </div>
+                    </div>
+                )))
             )
         }
 
