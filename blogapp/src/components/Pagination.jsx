8f641cbc8e9997a25fe30797dffc2f4eba96b470
Shivamya94 <yadavshivam90440@gmail.com>
2024-09-21 02:49:32 +0530

    ..

--- blogapp/src/components/Pagination.jsx
+++ blogapp/src/components/Pagination.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Pagination = () => {
+
+   const {page} = useContext(AppContext);
+
   return (
-    <div></div>
+    <div>
+      <div>
+        { page > 1 && 
+          <button onClick={() => handlePageChange(page-1)}>
+            Previous
+          </button>
+        }
+
+        {
+          <button>
+              Next 
+          </button>
+
+        }
+      </div>
+    </div>
   )
 }
 
