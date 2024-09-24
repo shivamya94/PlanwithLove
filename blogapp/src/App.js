17164d122e0b1f7239760a97da0c3001c31430b4
Shivamya94 <yadavshivam90440@gmail.com>
2024-09-24 12:01:24 +0530

    .

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -4,6 +4,7 @@ import Pagination from "./components/Pagination";
 import { useContext, useEffect } from "react";
 import { AppContext } from "./context/AppContext";
 import "./App.css"
+import {Route, Routes} from "react-router-dom"
 
 export default function App() {
   const {fetchBlogPosts} = useContext(AppContext);
@@ -12,6 +13,12 @@ export default function App() {
     fetchBlogPosts();
   },[]);
   return (
+    <Routes>
+      <Route path="/" element ={<Home/>} />
+      <Route path="/blog/:blogId" element ={<BlogPage/>} />
+      <Route path="/tags/:tag" element ={<TagPage/>} />
+      <Route path="/categories/:category" element ={<CategoryPage/>} />
+    </Routes>
    
   );
 }
