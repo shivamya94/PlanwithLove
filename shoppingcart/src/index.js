4400894cf517a5a45401e7b1d21521391da841c9
Shivamya94 <yadavshivam90440@gmail.com>
2024-10-08 12:11:04 +0530

    added

--- shoppingcart/src/index.js
+++ shoppingcart/src/index.js
@@ -5,18 +5,17 @@ import App from './App';
 import { store } from './redux/Store';
 import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
-
-
-
+import { Toaster } from 'react-hot-toast';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
 
-     <Provider store={store}>
-         <App />
-     </Provider>
-
-  </BrowserRouter>
+    <BrowserRouter>
+        <Provider store={store}>
+              <App />
+              <Toaster/>
+          </Provider>
+    </BrowserRouter>
+  
     
 );
 
--- shoppingcart/src/redux/Store.js
+++ shoppingcart/src/redux/Store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import CartSlice from "./Slices/CartSlice";
+import { CartSlice }from "./Slices/CartSlice";
 
 export const store = configureStore( {
     reducer: {
-        cart:CartSlice
+        cart:CartSlice.reducer,
     }
 });
